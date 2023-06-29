39b81ec80158c650303db36ea50b68ecd8050f81
Nander <nanderv@gmail.com>
2023-06-29 16:48:01 +0200

    move messageHandler to message.h file

--- traincontrol/traincommunications/coms.h
+++ traincontrol/traincommunications/coms.h
@@ -4,7 +4,6 @@
 typedef byte (*myRead)();
 typedef void (*myWrite)(char c);
 typedef int (*myAvailable)();
-typedef bool (*myHandler)(messageSlot *handleMessage, messageSlot *sendBack);
 
 
 struct twoHex {
@@ -26,21 +25,3 @@ struct comsChannel {
 };
 
 comsChannel comss[CHAN_IN_USE];
-messageSlot outboundMSG[CHAN_IN_USE];
-
-struct handler {
-  bool inUse;
-  byte typeHandled;
-  myHandler handler;
-};
-
-handler handlers[16];
-bool handleMessage(messageSlot *in, messageSlot *out){
-  out -> type = in -> type;
-  for(int i=0; i< MSG_LENGTH;i++){
-    out->content[i] =in -> content[i];
-  }
-  
-  out ->checkByte = in ->checkByte;
-  return true;
-}
--- traincontrol/traincommunications/message.h
+++ traincontrol/traincommunications/message.h
@@ -1,5 +1,5 @@
 # define MSG_LENGTH 6
-
+# define NUM_HANDLERS 16
 enum mode {
   type,
   content,
@@ -13,3 +13,36 @@ struct messageSlot {
   mode readState;
   int readCounter;
 };
+typedef bool (*myHandler)(messageSlot *handleMessage, messageSlot *sendBack);
+
+
+messageSlot outboundMSG[CHAN_IN_USE];
+
+struct handler {
+  bool inUse;
+  byte typeHandled;
+  myHandler handler;
+};
+
+handler handlers[NUM_HANDLERS];
+bool handleMessage(messageSlot *in, messageSlot *out){
+  out -> type = in -> type;
+  for(int i=0; i< MSG_LENGTH;i++){
+    out->content[i] =in -> content[i];
+  }
+  
+  out ->checkByte = in ->checkByte;
+  return true;
+}
+
+bool addHandler(byte typeHandled, myHandler func){
+  for (int i=0;i<NUM_HANDLERS;i++){
+    if (!(handlers[i].inUse)){
+        handlers[i].inUse = true;
+        handlers[i].typeHandled = type;
+        handlers[i].handler=func;
+        return true;
+    }
+  }
+  return false;
+}
