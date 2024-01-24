cbfda796362f51ce0f1da8b8c99f63031c351263
Nander Voortman <nander.voortman@nedap.com>
2024-01-24 17:33:39 +0100

    Add basic structure for sending messages to layout adapter.

--- internal/core/configurators.go
+++ internal/core/configurators.go
@@ -6,8 +6,15 @@ type Configurator func(*Core) error
 
 func WithFakeBridge() func(c *Core) error {
 	return func(c *Core) error {
-		c.CommandBridge = bridge.NewFakeBridge(&MessageAdapter{c: c})
+		c.commandBridge = bridge.NewFakeBridge(&MessageAdapter{c: c})
 
+		return nil
+	}
+}
+
+func WithTrackSwitch(id byte) func(c *Core) error {
+	return func(c *Core) error {
+		c.trackSwitches = append(c.trackSwitches, TrackSwitch{number: id})
 		return nil
 	}
 }
