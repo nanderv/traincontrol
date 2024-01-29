34419424ac34faec41c1f8d8a161626589a49607
Nander Voortman <nander@nander.net>
2024-01-29 23:11:01 +0100

    Clean up main func.

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,24 @@
 package main
 
 import (
-	"context"
 	"github.com/nanderv/traincontrol-prototype/internal/core"
 	"github.com/nanderv/traincontrol-prototype/internal/http_adapter"
 	"time"
 )
 
 func main() {
-	_, cancel := context.WithCancel(context.Background())
-	newCore, err := core.NewCore(core.WithFakeBridge(), core.WithTrackSwitch(1), core.WithTrackSwitch(2), core.WithTrackSwitch(3))
+	core, err := core.NewCore(core.WithFakeBridge(), core.WithTrackSwitch(1), core.WithTrackSwitch(2), core.WithTrackSwitch(3))
 	if err != nil {
 		return
 	}
 	go http_adapter.Init()
 
-	err = newCore.SetSwitchAction(1, true)
+	err = core.SetSwitchAction(1, true)
 	if err != nil {
 		panic(err)
 	}
-	newCore.SetSwitchAction(2, true)
-	newCore.SetSwitchAction(2, false)
+	core.SetSwitchAction(2, true)
+	core.SetSwitchAction(2, false)
 
 	time.Sleep(1 * time.Hour)
-
-	cancel()
-
 }
