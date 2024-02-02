c0eb53917a09427d64bca0c7806e7b7ec638cf88
Nander Voortman <nander@nander.net>
2024-02-03 00:10:29 +0100

    Moved core module to 'traintracks', renamed relevant files, refactored code to be cleaner.

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,8 @@ import (
 	"context"
 	"fmt"
 	"github.com/nanderv/traincontrol-prototype/internal/bridge"
-	"github.com/nanderv/traincontrol-prototype/internal/core"
-	"github.com/nanderv/traincontrol-prototype/internal/core/adapters"
-	"github.com/nanderv/traincontrol-prototype/internal/hwconfig"
-	hwAdapters "github.com/nanderv/traincontrol-prototype/internal/hwconfig/adapters"
+	"github.com/nanderv/traincontrol-prototype/internal/traintracks"
+	"github.com/nanderv/traincontrol-prototype/internal/traintracks/adapters"
 	"github.com/nanderv/traincontrol-prototype/internal/web"
 	"log/slog"
 	"os"
@@ -17,14 +15,15 @@ import (
 func main() {
 	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
 
-	c, err := core.NewCore(core.WithTrackSwitch(1), core.WithTrackSwitch(2), core.WithTrackSwitch(3))
+	c, err := traintracks.NewCore(traintracks.WithTrackSwitch(1), traintracks.WithTrackSwitch(2), traintracks.WithTrackSwitch(3))
 
 	bridg := bridge.NewSerialBridge()
 	go bridg.Handle()
+
 	adapters.NewMessageAdapter(c, bridg)
 
-	hwConf := hwconfig.HwConfigurator{}
-	hwAdapters.NewMessageAdapter(&hwConf, bridg)
+	//hwConf := hwconfig.HwConfigurator{}
+	//hwAdapters.NewMessageAdapter(&hwConf, bridg)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -38,6 +37,17 @@ func main() {
 			fmt.Println(err)
 		}
 	}()
+
+	err = c.SetSwitchAction(1, true)
+	if err != nil {
+		slog.Error("Could not set switch", "error", err)
+		//return
+	}
+	err = c.SetSwitchAction(1, true)
+	if err != nil {
+		slog.Error("Could not set switch", "error", err)
+		//return
+	}
 
 	time.Sleep(1 * time.Hour)
 }
