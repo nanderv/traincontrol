defb244ae40613d599eebcdb827df7e2eaa5af36
Nander Voortman <nander@nander.net>
2024-02-02 12:20:23 +0100

    Add most basic HWConfig system.

---
+++ internal/hwconfig/domain/subcodes/types.go
@@ -0,0 +1,10 @@
+package subcodes
+
+const Announce = 1
+const AckAnnounce = 2
+const ConfirmAckAnnounce = 3
+
+const LightOn = 16
+const AckLightOn = 17
+const LightOff = 18
+const AckLightOff = 19
--- internal/hwconfig/hwcon.go
+++ internal/hwconfig/hwcon.go
@@ -1,1 +1,20 @@
 package hwconfig
+
+import (
+	"github.com/nanderv/traincontrol-prototype/internal/bridge/domain"
+)
+
+type node struct {
+	mac  [3]byte
+	addr byte
+}
+type HwConfigurator struct {
+	nodes []node
+
+	bridges []MessageSender[domain.Msg]
+}
+
+func (c *HwConfigurator) AddCommandBridge(cc MessageSender[domain.Msg]) {
+	c.bridges = append(c.bridges, cc)
+	return
+}
