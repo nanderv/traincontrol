dfcaa5ed8583cf52f9a58f5e4740e68b4ce5c785
Nander Voortman <nander@nander.net>
2024-02-03 15:44:53 +0100

    Simplify abstractions.

--- internal/bridge/ports.go
+++ internal/bridge/ports.go
@@ -1,6 +1,9 @@
 package bridge
 
-import "github.com/nanderv/traincontrol-prototype/internal/bridge/domain"
+import (
+	"github.com/nanderv/traincontrol-prototype/internal/bridge/domain"
+	"time"
+)
 
 type MessageReceiver interface {
 	Receive(msg domain.Msg) error
@@ -9,4 +12,5 @@ type MessageReceiver interface {
 type Bridge interface {
 	AddReceiver(MessageReceiver)
 	Send(domain.Msg) error
+	SendMessageWithConfirmationAndRetries(msg domain.Msg, checker func(msg domain.Msg) bool, timeout time.Duration, retries int) error
 }
