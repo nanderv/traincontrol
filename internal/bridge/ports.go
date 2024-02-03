0e7285b35f1798bd647310fa82c6a4543d4cb3c9
Nander Voortman <nander@nander.net>
2024-02-03 11:59:22 +0100

    Simplify interfaces.

--- internal/bridge/ports.go
+++ internal/bridge/ports.go
@@ -5,3 +5,8 @@ import "github.com/nanderv/traincontrol-prototype/internal/bridge/domain"
 type MessageReceiver interface {
 	Receive(msg domain.Msg) error
 }
+
+type Bridge interface {
+	AddReceiver(MessageReceiver)
+	Send(domain.Msg) error
+}
