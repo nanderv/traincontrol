332e5fc368f8ec8ad984d0fd82f84fe245680c19
Nander Voortman <nander@nander.net>
2024-02-04 12:18:33 +0100

    Clean up microcontroller code.

--- microcontroller/message.h
+++ microcontroller/message.h
@@ -4,25 +4,25 @@
 
 
 struct messageSlot {
-  byte type;
-  byte content[MSG_LENGTH];
-  byte checkByte;
+    byte type;
+    byte content[MSG_LENGTH];
+    byte checkByte;
 };
 
 typedef bool (*myHandler)(messageSlot *handleMessage, messageSlot *sendBack);
 
 void setCheckByte(messageSlot *m){
-  byte check = m->type;
+    byte check = m->type;
     for(int i=0; i< MSG_LENGTH;i++){
-      check = check ^ m->content[i];
+        check = check ^ m->content[i];
     }
-  m->checkByte = check;
+    m->checkByte = check;
 }
 
 struct handler {
-  bool inUse;
-  byte typeHandled;
-  myHandler handler;
+    bool inUse;
+    byte typeHandled;
+    myHandler handler;
 };
 
 handler handlers[NUM_HANDLERS];
@@ -40,43 +40,31 @@ bool handleMessage(messageSlot *in, messageSlot *out){
 }
 
 bool addHandler(byte typeHandled, myHandler func){
-  for (int i=0;i<NUM_HANDLERS;i++){
-    if (!(handlers[i].inUse)){
-        handlers[i].inUse = true;
-        handlers[i].typeHandled = typeHandled;
-        handlers[i].handler=func;
-        return true;
+    for (int i=0;i<NUM_HANDLERS;i++){
+        if (!(handlers[i].inUse)){
+            handlers[i].inUse = true;
+            handlers[i].typeHandled = typeHandled;
+            handlers[i].handler=func;
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 bool echoEcho(messageSlot *handleMessage, messageSlot *sendBack){
     sendBack -> type = handleMessage -> type;
-  for(int i=0; i< MSG_LENGTH;i++){
-    sendBack->content[i] =handleMessage -> content[i];
-  }
-  
-  sendBack ->checkByte = handleMessage ->checkByte;
-  return true;
-}
-
-bool flashingLights(messageSlot *handleMessage, messageSlot *sendBack){
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(handleMessage->content[0]*10);
-  digitalWrite(LED_BUILTIN, LOW);
-  sendBack ->type= 2;
     for(int i=0; i< MSG_LENGTH;i++){
-    sendBack->content[i] =handleMessage -> content[i];
-  }
-  setCheckByte(sendBack);
-    return false;
+        sendBack->content[i] =handleMessage -> content[i];
+    }
+
+    sendBack ->checkByte = handleMessage ->checkByte;
+    return true;
 }
 
 bool sendAck(messageSlot *handleMessage, messageSlot *sendBack, byte id){
-  sendBack->type = id;
-  for (int i=0;i<MSG_LENGTH;i++){
-    sendBack->content[i] = handleMessage->content[i];
-  }
-  setCheckByte(sendBack);
-  return true;
+    sendBack->type = id;
+    for (int i=0;i<MSG_LENGTH;i++){
+        sendBack->content[i] = handleMessage->content[i];
+    }
+    setCheckByte(sendBack);
+    return true;
 }
---
+++ microcontroller/startup.h
@@ -0,0 +1,11 @@
+# define ADDR_BROADCAST 1
+# define ADDR_SET 2
+
+
+# define RESTART_CODE 254
+
+
+
+# define STATUS_NEW 0
+# define STATUS_PENDING 1
+# define STATUS_RUNNING 255
