c16de5bad57ff92169a50ffb8bb603be30a3aac1
Nander Voortman <nander@nander.net>
2024-02-04 21:31:08 +0100

    Add basic startup ritual.

--- microcontroller/microcontroller.ino
+++ microcontroller/microcontroller.ino
@@ -1,10 +1,16 @@
-# define Coms0 Serial
-# define Coms1 Serial1
-# define Coms3 Serial3
-# define Coms4 Serial4
-# define Coms5 Serial5
-# define CHAN_IN_USE 6
-# define ERROR_TYPE 255
+#define Coms0 Serial
+#define Coms1 Serial1
+#define Coms3 Serial3
+#define Coms4 Serial4
+#define Coms5 Serial5
+#define CHAN_IN_USE 6
+
+# define EEPROM_READ  3
+# define EEPROM_READ_RETURN  4
+# define EEPROM_WRITE 5
+# define EEPROM_WRITE_RETURN 6
+
+#define ERROR_TYPE 255
 #include "cmds.h"
 #include "coms.h"
 #include "metaControl.h"
@@ -13,64 +19,61 @@
 #include "startup.h"
 
 void setup() {
-  addHandler(0,metaControl );
-  addHandler(1, flashingLights);
-  addHandler(SWITCH_TYPE, controlSwitch);
-  addHandler(SECTOR_TYPE, controlSector);
+    addHandler(1, flashingLights);
+    addHandler(SWITCH_TYPE, controlSwitch);
+    addHandler(SECTOR_TYPE, controlSector);
 
-  Coms0.begin(115200);
-  Coms1.begin(9600);
-  Coms3.begin(9600);
-  Coms4.begin(9600);
-  Coms5.begin(9600);
-  LoadMemory();
+    Coms0.begin(115200);
+    Coms1.begin(9600);
+    Coms3.begin(9600);
+    Coms4.begin(9600);
+    Coms5.begin(9600);
+    LoadMemory();
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  
-  comms[0].write = coms0Write;
-  comms[0].available = coms0Available;
-  comms[0].read = coms0Read;
-  comms[0].inUse = true;
-  comms[1].write = coms1Write;
-  comms[1].available = coms1Available;
-  comms[1].read = coms1Read;
-  comms[1].inUse = true;
-  comms[2].write = coms3Write;
-  comms[2].available = coms3Available;
-  comms[2].read = coms3Read;
-  comms[2].inUse = true;
-  comms[3].write = coms4Write;
-  comms[3].available = coms4Available;
-  comms[3].read = coms4Read;
-  comms[3].inUse = true;
-  comms[4].write = coms5Write;
-  comms[4].available = coms5Available;
-  comms[4].read = coms5Read;
-  comms[4].inUse = true;
+    pinMode(LED_BUILTIN, OUTPUT);
 
+    comms[0].write = coms0Write;
+    comms[0].available = coms0Available;
+    comms[0].read = coms0Read;
+    comms[0].inUse = true;
+    comms[1].write = coms1Write;
+    comms[1].available = coms1Available;
+    comms[1].read = coms1Read;
+    comms[1].inUse = true;
+    comms[2].write = coms3Write;
+    comms[2].available = coms3Available;
+    comms[2].read = coms3Read;
+    comms[2].inUse = true;
+    comms[3].write = coms4Write;
+    comms[3].available = coms4Available;
+    comms[3].read = coms4Read;
+    comms[3].inUse = true;
+    comms[4].write = coms5Write;
+    comms[4].available = coms5Available;
+    comms[4].read = coms5Read;
+    comms[4].inUse = true;
 }
 
-int controllerStatus ;
+int controllerStatus;
 int sleepCount;
 messageSlot startupSendSlot;
 
 void loop() {
-  for (int i = 0; i < CHAN_IN_USE; i++) {
+    for (int i = 0; i < CHAN_IN_USE; i++) {
     handleChannel(i);
-  }
-  if (controllerStatus != STATUS_RUNNING){
-    if(sleepCount ==0){
-      digitalWrite(LED_BUILTIN, HIGH);
-      handleNonRunningState();
     }
-    if(sleepCount == 1000){
-      digitalWrite(LED_BUILTIN, LOW);
+    if (sleepCount == 0) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        handleNonRunningState();
     }
-    sleepCount ++;
-    if (sleepCount == 5000){
-      sleepCount = 0;
+    if (sleepCount == 1000) {
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+    sleepCount++;
+    if (sleepCount == 5000) {
+        sleepCount = 0;
     }
     delay(1);
-  }
-  Serial.flush();
+
+    Serial.flush();
 }
