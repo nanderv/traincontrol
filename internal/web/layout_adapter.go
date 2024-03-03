6adff23f5e7875f490f290b2922d2276d6761da9
Simon Struck <simon@simonscode.org>
2024-03-03 13:31:02 +0100

    Remove semantic and switch HTTP server to echo

--- internal/web/layout_adapter.go
+++ internal/web/layout_adapter.go
@@ -3,6 +3,7 @@ package web
 import (
 	"context"
 	"encoding/json"
+	"github.com/labstack/echo/v4"
 	"github.com/nanderv/traincontrol-prototype/internal/traintracks"
 	"github.com/nanderv/traincontrol-prototype/internal/traintracks/domain"
 	"io"
@@ -40,4 +41,11 @@ func (l *LayoutAdapter) Handle(ctx context.Context) {
 			}
 		}
 	}
+}
+
+func (l *LayoutAdapter) SetSwitch(c echo.Context) error {
+	switchID := c.Request().PostFormValue("switchID")
+	direction := c.Request().PostFormValue("direction")
+	slog.Info("Set switch", "SwitchID", switchID, "Direction", direction)
+	return l.c.SetSwitchDirection(switchID, direction == "true")
 }
