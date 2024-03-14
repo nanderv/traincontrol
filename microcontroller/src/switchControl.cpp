51766407ff22af5c6752d54fe2a9027100a97d70
Nander Voortman <nander@nander.net>
2024-03-14 20:38:57 +0100

    Fix edge case in data parsing.

--- microcontroller/src/switchControl.cpp
+++ microcontroller/src/switchControl.cpp
@@ -2,7 +2,7 @@
 
 bool controlSwitch(messageSlot *handleMessage, messageSlot *sendBack) {
   // If the message is not for me
-  if (MAC[0] != handleMessage->content[0] && MAC[1] != handleMessage->content[1] && MAC[2] != handleMessage->content[2]){
+  if (MAC[0] != handleMessage->content[0] || MAC[1] != handleMessage->content[1] || MAC[2] != handleMessage->content[2]){
     return false;
   }
 
