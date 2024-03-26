36d00e71a4422d19a5b24a207645c3c65fb7efb6
Nander Voortman <nander.voortman@nedap.com>
2024-03-26 18:42:54 +0100

    add terminal prototype

--- cmd/term/main.go
+++ cmd/term/main.go
@@ -1,1 +1,68 @@
-package term
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/nanderv/traincontrol-prototype/datasets/test"
+	"github.com/nanderv/traincontrol-prototype/internal/hardware"
+	"github.com/nanderv/traincontrol-prototype/internal/serialbridge"
+	traintracks2 "github.com/nanderv/traincontrol-prototype/internal/serialbridge/adapters"
+	"github.com/rivo/tview"
+	"log/slog"
+)
+
+type NulWriter struct{}
+
+func (w *NulWriter) Write([]byte) (int, error) { return 0, nil }
+func main() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(&NulWriter{}, nil)))
+	brdg := "fake"
+	flag.StringVar(&brdg, "bridge", "fake", "Set which hardware brdige to use (fake or serial)")
+	if brdg != "fake" && brdg != "serial" {
+		panic("Bridge ain't real")
+	}
+	layStr := "test"
+	flag.StringVar(&layStr, "layout", "test", "Set which layout to use")
+	lay := test.GetBaseLayout()
+	c, err := hardware.NewTrackService(lay)
+	if err != nil {
+		panic(err)
+	}
+
+	var b serialbridge.Bridge
+	if brdg == "fake" {
+		b = serialbridge.NewFakeBridge()
+	}
+	if brdg == "serial" {
+		b = serialbridge.NewSerialBridge()
+	}
+	traintracks2.NewMessageAdapter(c, b)
+	cha := c.AddNewReturnChannel()
+	app := tview.NewApplication()
+	form := tview.NewForm()
+	buttons := make(map[string]*tview.Button)
+	for _, sw := range lay.TrackSwitches {
+		s := sw
+		but := tview.NewButton(fmt.Sprintf("ST %s %v", s.Name, s.Direction))
+		fmt.Println(but)
+		buttons[s.Name] = but
+	}
+	go func() {
+		select {
+		case sta := <-*cha:
+			lay = sta
+			i := 0
+			for _, sw := range sta.TrackSwitches {
+				s := sw
+
+				form.GetButton(i).SetLabel(fmt.Sprintf("ST %s %v", s.Name, s.Direction))
+
+				i++
+			}
+		}
+	}()
+	form.SetBorder(true).SetTitle("Enter some data").SetTitleAlign(tview.AlignLeft)
+	if err := app.SetRoot(form, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
+}
---
+++ datasets/test/init.go
@@ -0,0 +1,230 @@
+package test
+
+import (
+	hwDomain "github.com/nanderv/traincontrol-prototype/internal/hardware/domain"
+	"github.com/nanderv/traincontrol-prototype/internal/serialbridge/domain"
+)
+
+func GetBaseLayout() hwDomain.HardwareState {
+	lay := hwDomain.NewHardwareState()
+	lay.WithTrackSwitch(hwDomain.TrackSwitch{
+		Mac:       domain.Mac{20, 141, 142},
+		PortID:    0,
+		LeftPin:   2,
+		RightPin:  3,
+		Name:      "1",
+		Direction: false,
+	})
+	lay.WithTrackSwitch(hwDomain.TrackSwitch{
+		Mac:       domain.Mac{20, 141, 142},
+		PortID:    0,
+		LeftPin:   13,
+		RightPin:  14,
+		Name:      "b",
+		Direction: false,
+	})
+	lay.WithTrackSwitch(hwDomain.TrackSwitch{
+		Mac:       domain.Mac{20, 140, 204},
+		PortID:    0,
+		LeftPin:   15,
+		RightPin:  16,
+		Name:      "sl",
+		Direction: false,
+	})
+	lay.WithTrackSwitch(hwDomain.TrackSwitch{
+		Mac:       domain.Mac{22, 229, 217},
+		PortID:    0,
+		LeftPin:   13,
+		RightPin:  14,
+		Name:      "sl2",
+		Direction: false,
+	})
+	lay.WithBlock(hwDomain.Block{
+		Name: "forest_to_shadow",
+		Segments: []hwDomain.Segment{
+			{
+				Line: hwDomain.Line{
+					StartX: 170,
+					StartY: 400,
+					EndX:   200,
+					EndY:   400,
+				},
+				Enabled: true,
+			},
+			{
+				Line: hwDomain.Line{
+					StartX: 170,
+					StartY: 380,
+					EndX:   200,
+					EndY:   400,
+				},
+				Enabled: false,
+			},
+			{
+				Line: hwDomain.Line{
+					StartX: 200,
+					StartY: 400,
+					EndX:   400,
+					EndY:   400,
+				},
+				Enabled: true,
+			},
+		},
+
+		Enabled: false,
+	})
+
+	lay.WithBlock(hwDomain.Block{
+		Name: "forest_siding",
+		Segments: []hwDomain.Segment{
+
+			{
+				Line: hwDomain.Line{
+					StartX: 110,
+					StartY: 340,
+					EndX:   167,
+					EndY:   378,
+				},
+				Enabled: true,
+			},
+		},
+
+		Enabled: false,
+	})
+	lay.WithBlock(hwDomain.Block{
+		Name: "forest_curve",
+		Segments: []hwDomain.Segment{
+
+			{
+				Line: hwDomain.Line{
+					StartX: 110,
+					StartY: 400,
+					EndX:   167,
+					EndY:   400,
+				},
+				Enabled: true,
+			},
+		},
+
+		Enabled: false,
+	})
+
+	lay.WithBlock(hwDomain.Block{
+		Name: "forest_to_main",
+		Segments: []hwDomain.Segment{
+
+			{
+				Line: hwDomain.Line{
+					StartX: 50,
+					StartY: 400,
+					EndX:   107,
+					EndY:   400,
+				},
+				Enabled: true,
+			},
+		},
+
+		Enabled: false,
+	})
+	lay.WithBlock(hwDomain.Block{
+		Name: "shadow_left",
+		Segments: []hwDomain.Segment{
+
+			{
+				Line: hwDomain.Line{
+					StartX: 410,
+					StartY: 400,
+					EndX:   450,
+					EndY:   400,
+				},
+				Enabled: true,
+			},
+			{
+				Line: hwDomain.Line{
+					StartX: 450,
+					StartY: 400,
+					EndX:   480,
+					EndY:   400,
+				},
+				Enabled: true,
+			},
+			{
+				Line: hwDomain.Line{
+					StartX: 450,
+					StartY: 400,
+					EndX:   480,
+					EndY:   370,
+				},
+				Enabled: true,
+			},
+		},
+
+		Enabled: false,
+	})
+	lay.WithBlock(hwDomain.Block{
+		Name: "shadow_top_l",
+		Segments: []hwDomain.Segment{
+
+			{
+				Line: hwDomain.Line{
+					StartX: 483,
+					StartY: 370,
+					EndX:   557,
+					EndY:   370,
+				},
+				Enabled: true,
+			},
+		},
+		Enabled: false,
+	})
+	lay.WithBlock(hwDomain.Block{
+		Name: "shadow_bottom_l",
+		Segments: []hwDomain.Segment{
+
+			{
+				Line: hwDomain.Line{
+					StartX: 483,
+					StartY: 400,
+					EndX:   557,
+					EndY:   400,
+				},
+				Enabled: true,
+			},
+		},
+		Enabled: false,
+	})
+	lay.WithBlock(hwDomain.Block{
+		Name: "shadow_top_r",
+		Segments: []hwDomain.Segment{
+
+			{
+				Line: hwDomain.Line{
+					StartX: 560,
+					StartY: 370,
+					EndX:   634,
+					EndY:   370,
+				},
+				Enabled: true,
+			},
+		},
+		Enabled: false,
+	})
+	lay.WithBlock(hwDomain.Block{
+		Name: "shadow_bottom_r",
+		Segments: []hwDomain.Segment{
+
+			{
+				Line: hwDomain.Line{
+					StartX: 560,
+					StartY: 400,
+					EndX:   634,
+					EndY:   400,
+				},
+				Enabled: true,
+			},
+		},
+		Enabled: false,
+	})
+
+	return lay
+}
